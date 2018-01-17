2939098a0a6deb0e7f0cf879288c2dc21ec9dd57
Ellen Fawkes <fawkes@ttc.cz>
2018-01-17 17:28:08 +0100

    Refactored integration tests

--- tests/integration/users.test.js
+++ tests/integration/users.test.js
@@ -12,6 +12,13 @@ var samples = {
       activated: true
     }
   ],
+  user: {
+    _id: "sdfdfg46sd5g4f",
+    login: "ashley",
+    name: "Lovely Ashley",
+    role: "admin",
+    activated: true
+  },
   created: {
     _id: "abcdefg",
     login: "elise",
@@ -21,7 +28,7 @@ var samples = {
   }
 }
 
-test("Get users", async t => {
+test.before("Setup nock", t => {
   nock("http://localhost:6060")
     .get("/users")
     .reply(200, {
@@ -31,6 +38,31 @@ test("Get users", async t => {
       },
       content: samples.users
     })
+    .get("/users/@ashley")
+    .reply(200, {
+      status: {
+        code: "OK",
+        message: "OK"
+      },
+      content: samples.user
+    })
+    .post("/users", { login: "elise", password: "pass321abc", name: "Elise Bauman" })
+    .reply(201, {
+      status: {
+        code: "CREATED",
+        message: "Content created"
+      },
+      content: samples.created
+    }).get("/users/@natvanlis")
+    .reply(404, {
+      status: {
+        code: "NOT_FOUND",
+        message: "Resource not found"
+      }
+    })
+})
+
+test("Get users", async t => {
   var caloriosa = Caloriosa.createApiClient()
   var users = await caloriosa.api.users.get(/* {filter: {login: "ashley", bool: true}} */)
   t.is(users.status.code, "OK")
@@ -38,17 +70,25 @@ test("Get users", async t => {
   t.deepEqual(users.content, samples.users)
 })
 
+test("Get user by login", async t => {
+  var caloriosa = Caloriosa.createApiClient()
+  var user = await caloriosa.api.users("@ashley").get()
+  t.is(user.status.code, Caloriosa.ApiStatuses.OK)
+  t.is(user.status.message, "OK")
+  t.deepEqual(user.content, samples.user)
+})
+
 test("Create user", async t => {
-  nock("http://localhost:6060")
-    .post("/users", { login: "elise", password: "pass321abc", name: "Elise Bauman" })
-    .reply(200, {
-      status: {
-        code: "OK",
-        message: "OK"
-      },
-      content: samples.created
-    })
   var caloriosa = Caloriosa.createApiClient()
   var created = await caloriosa.api.users.post({ login: "elise", password: "pass321abc", name: "Elise Bauman" })
+  t.is(created.status.code, Caloriosa.ApiStatuses.CREATED)
+  t.is(created.status.message, "Content created")
   t.deepEqual(created.content, samples.created)
+})
+
+test("User not found", async t => {
+  var caloriosa = Caloriosa.createApiClient()
+  var error = await t.throws(caloriosa.api.users("@natvanlis").get(), Caloriosa.CaloriosaApiError)
+  t.is(error.statusCode, 404)
+  t.is(error.code, Caloriosa.ApiStatuses.NOT_FOUND)
 })
