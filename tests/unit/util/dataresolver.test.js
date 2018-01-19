88b38fa0e5aa826a1e874d06467e9ef525b3ad27
Ellen Fawkes <fawkes@ttc.cz>
2018-01-19 09:30:51 +0100

    Covered DataResolver

--- tests/unit/util/dataresolver.test.js
+++ tests/unit/util/dataresolver.test.js
@@ -12,11 +12,24 @@ test("resolve UID", t => {
     activated: true,
     role: "admin"
   }
+  var user2 = {
+    uid: "6fc3d11025a09c365cd4ca45",
+    login: "natvanlis",
+    role: "member"
+  }
   var uid = user._id
   var someObject = {foo: "bar"}
   var number = 15
   t.is(DataResolver.resolveUid(user), "5a23d11025a09c281cd3ca13")
+  t.is(DataResolver.resolveUid(user2), "6fc3d11025a09c365cd4ca45")
   t.is(DataResolver.resolveUid(uid), "5a23d11025a09c281cd3ca13")
   t.is(DataResolver.resolveUid(someObject), null)
   t.is(DataResolver.resolveUid(number), null)
+})
+
+test("Can't instantiate DataResolver (static)", t => {
+  var error = t.throws(() => {
+    var resolver = new DataResolver()
+  }, Error)
+  t.is(error.message, "Can't instantiate static class!")
 })
